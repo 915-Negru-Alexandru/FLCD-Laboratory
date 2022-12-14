13d9165b6d3c9903a7ab46b1ca3dbc3aad143229
BlackAlexander <72079413+915-Negru-Alexandru@users.noreply.github.com>
2022-12-14 13:01:10 +0200

    Started parsing table

--- Lab 5/main.py
+++ Lab 5/main.py
@@ -7,6 +7,8 @@ class Grammar:
         self.production_rules = []
         self.starting_symbol = None
 
+        self.parsing_table = None
+
     def read_grammar(self, file):
         try:
             fin = open(file, 'r')
@@ -46,13 +48,84 @@ class Grammar:
             right_side = splitted[1].split(" | ")
             new_rule[1] = right_side
 
+        self.create_table()
+
         self.print_non_terminals()
         self.print_terminals()
         self.print_set_of_productions()
         self.print_productions_for_terminal("whilestmt")
         self.print_productions_for_terminal("RELATION")
         self.check_cfg()
-        self.first()
+        # self.first()
+        self.print_table()
+
+    def create_table(self):
+        self.parsing_table = {}
+        for symbol in self.non_terminals:
+            self.parsing_table[symbol] = {}
+            for col_symbol in self.terminals:
+                self.parsing_table[symbol][col_symbol] = " "
+            self.parsing_table[symbol]['$'] = " "
+
+        for symbol in self.terminals:
+            self.parsing_table[symbol] = {}
+            for col_symbol in self.terminals:
+                self.parsing_table[symbol][col_symbol] = " "
+            self.parsing_table[symbol]['$'] = " "
+
+        self.parsing_table['$'] = {}
+        for col_symbol in self.terminals:
+            self.parsing_table['$'][col_symbol] = " "
+
+        for line in self.parsing_table:
+            for column in self.parsing_table[line]:
+                if line == column:
+                    self.parsing_table[line][column] = 'POP'
+        self.parsing_table['$']['$'] = "ACC"
+
+
+    def print_table(self):
+        print("------------- PARSING TABLE ----------------")
+        for line in self.parsing_table:
+            print(str(line) + ": " + str(self.parsing_table[line]))
+        print('\n')
+        table_line = "+---+"
+        for column in self.parsing_table[self.non_terminals[0]]:
+            table_line += "--------+"
+        print(table_line)
+        print("| \ ", end="")
+        for column in self.parsing_table[self.non_terminals[0]]:
+            print("|", end="")
+            result = column
+            for i in range(0, int((8-len(result))/2)):
+                print(" ", end="")
+            print(result, end="")
+            for i in range(0, int((8-len(result))/2) + 1):
+                print(" ", end="")
+        print("|")
+        print(table_line)
+        for line in self.parsing_table:
+            print("|", end="")
+            line_title = line
+            if len(line_title) == 1:
+                print(" " + line_title + " ", end="")
+            if len(line_title) == 3:
+                print(line_title, end="")
+            for column in self.parsing_table[line]:
+                print("|", end="")
+                result = self.parsing_table[line][column]
+                for i in range(0, int((8-len(result))/2)):
+                    print(" ", end="")
+                print(result, end="")
+                for i in range(0, int((8-len(result))/2) + 1):
+                    print(" ", end="")
+            print("|")
+            print(table_line)
+
+
+        print("\n\n\n")
+
+        print("--------------------------------------------")
 
     def print_non_terminals(self):
         print("------------- NON TERMINALS ----------------")
